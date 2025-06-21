40faa05dc9dcf8434e634927007640c9ac258c7b
Mingu Kang <mg.kang0518@gmail.com>
2025-06-21 17:46:23 +0930

    refactor: update Product component

--- src/Product.jsx
+++ src/Product.jsx
@@ -9,7 +9,7 @@ function Product() {
   let [products, setProducts] = useState([]);
 
     useEffect(() => {
-      axios.get('https://mingukang-coding.github.io/react_prac/my-json-data')
+      axios.get(`https://mingukang-coding.github.io/react_prac/my-json-data?timestamp=${Date.now()}`)
         .then((res) => {
           setProducts(res.data);
         })
@@ -22,17 +22,14 @@ function Product() {
 
       <Row xs={1} md={2} className="g-4" style={{ paddingTop: '24px' }}>
         
-        { products.map((_, idx) => (
+        { products.map(( a, idx) => (
           <Col key={idx} >
             <Card>
                 <Card.Img variant="top" src="holder.js/100px160" />
                 <Card.Body>
-                  <Card.Title>Card title</Card.Title>
-                  <Card.Text>
-                    This is a longer card with supporting text below as a natural
-                    lead-in to additional content. This content is a little bit
-                    longer.
-                  </Card.Text>
+                  <Card.Title>{a.name}</Card.Title>
+                  <Card.Text>product category : {a.category}</Card.Text>
+                  <Card.Text>{a.cost}</Card.Text>
                   <button className='btn btn-danger' style={{display : 'centre'}}>Order now</button>
                 </Card.Body>
               </Card>
