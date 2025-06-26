c77594b50e59c06d64fcf2eca215c44316a2c015
Mingu Kang <mg.kang0518@gmail.com>
2025-06-27 01:29:10 +0930

    refactor: manage server data through shared state using Redux

--- src/Product.jsx
+++ src/Product.jsx
@@ -3,30 +3,36 @@ import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import axios from 'axios';
 import { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { setProducts } from './store/productSlice';
+
 
 function Product() {
+  const dispatch = useDispatch();
+
   const [baseImgUrl, setbaseImgUrl] = useState('https://mingukang-coding.github.io/react_prac/product_img/');
 
-  let [products, setProducts] = useState([]);
+  // let [products, setProducts] = useState([]);
 
     useEffect(() => {
       axios.get(`https://mingukang-coding.github.io/react_prac/my-json-data?timestamp=${Date.now()}`)
         .then((res) => {
-          setProducts(res.data);
+          dispatch(setProducts(res.data));
         })
         .catch(() => {
           console.log('Error : fail to load data..');
         });
     }, []);
-    
+
+    const products = useSelector((state) => state.products);
+
     return (
 
       <Row xs={1} md={4} className="g-4" style={{ margin: 'auto 5px'}}>
         
-        { products.map(( a, idx) => (
+        { Array.isArray(products) && products.map((a, idx) => (
           <Col key={idx} >
             <Card onClick={()=>{
-
             }}>
                 <Card.Img variant="top"   
                   style={{ width: '300px', height: '300px', objectFit: 'cover' }} 
